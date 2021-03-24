0c5c7f1bba01c1ab7365f9b27fd21f418b520f1f
Meenhaz Foysol <meenhaz@Meenhazs-Air.home>
2021-03-24 13:45:43 -0400

    created profile page to test branch function

---
+++ src/test/java/TestNGSession/profilePage.java
@@ -0,0 +1,11 @@
+package TestNGSession;
+
+public class profilePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Test for git branches");
+	}
+
+}
